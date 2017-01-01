66a17a2c5eefc29df1f8cff116828cc20f4a25d6
Sergio del Valle <svsalvador@gmail.com>
2017-01-02 00:46:26 +0100

    Added action to index form

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,6 +5,15 @@ export default Ember.Controller.extend({
   emailAddress: '',
 
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: Ember.computed.not('isValid')
+  isDisabled: Ember.computed.not('isValid'),
+
+  actions: {
+
+    saveInvitation() {
+      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
 
 });
