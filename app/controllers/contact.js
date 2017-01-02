0929fd6ec4dc1a0b1ae4cfc26d271ef06dfbabcd
Sergio del Valle <svsalvador@gmail.com>
2017-01-02 23:11:20 +0100

    Save contacts to database

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -14,9 +14,21 @@ export default Ember.Controller.extend({
   actions: {
 
     sendMessage() {
-      this.set('responseMessage', 'We got your message and we’ll get in touch soon');
-      this.set('emailAddress', '');
-      this.set('message', '');
+
+      const email = this.get('emailAddress');
+      const message = this.get('message');
+
+      const newContact = this.store.createRecord('contact', {
+        email: email,
+        message: message
+      });
+
+      newContact.save().then((response) => {
+        this.set('responseMessage', 'We got your message and we’ll get in touch soon');
+        this.set('emailAddress', '');
+        this.set('message', '');
+      });
+
     }
   }
 });
