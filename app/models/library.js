86d8eb3c24a4ebee2dcc981aa0120019ae7c6d5c
Sergio del Valle <svsalvador@gmail.com>
2017-01-03 01:13:01 +0100

    Refactored libraries form into components

--- app/models/library.js
+++ app/models/library.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   address: DS.attr('string'),
-  phone: DS.attr('string')
+  phone: DS.attr('string'),
+  isValid: Ember.computed.notEmpty('name')
 });
